a55a4b6b1dedfce15f50fe3413ca4e3bf7d5e63c
GuiMedina1 <guilherme.medinag@gmail.com>
2025-03-17 15:55:22 -0300

    Add files via upload

---
+++ teste v1.0.js
@@ -0,0 +1,8 @@
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaDeAmigos = []
+let quantidadeDeAmigos = listaDeAmigos.length
+let amigo = document.querySelector('input').value
+listaDeAmigos.includes(amigo)
+
+let escolhido = parseInt(Math.random() * quantidadeDeAmigos)
+prompt = listaDeAmigos[escolhido]
